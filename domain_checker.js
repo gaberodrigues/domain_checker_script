1f119c541086d96dac6aaf2d448e9236d073f1fd
gaberodrigues <42834890+gaberodrigues@users.noreply.github.com>
2018-08-30 21:20:50 +1000

    Initial commit

---
+++ domain_checker.js
@@ -0,0 +1,25 @@
+var link = document.getElementById('Submit');
+var i = 0, y=0, howManyTimes = 1000000;
+var audio = new Audio('https://www.soundjay.com/phone/telephone-ring-01a.mp3');
+
+function f() {
+    i++;
+    if( i < howManyTimes ){
+    link.click();
+        setTimeout( f, 300000 );
+    }
+}
+f();
+function g() {
+    y++;
+    if( y < howManyTimes ){
+var results= document.getElementById("results");
+var substr = results.innerHTML;   
+if(substr.includes("Congratulations")){
+	audio.play();
+        alert("Available");
+    }
+        setTimeout( g, 5000 );
+    }
+}
+g();
